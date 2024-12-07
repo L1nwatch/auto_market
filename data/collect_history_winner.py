78c868629ca36ce357a62d407a14dc60874488bf
L1nwatch <490772448@qq.com>
2024-12-06 20:23:11 -0800

    [2024-12-06] write a script to extract history number

--- data/collect_history_winner.py
+++ data/collect_history_winner.py
@@ -6,11 +6,13 @@
 import requests
 import json
 from tqdm import tqdm
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
 __author__ = '__L1n__w@tch'
 
-if __name__ == "__main__":
+
+def history_year():
     data = {}
     history_winner_file = "history_winner.json"
 
@@ -38,3 +40,38 @@ if __name__ == "__main__":
 
         with open(history_winner_file, "w") as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+def get_current_results_date():
+    result = list()
+    for month in range(1, 13):
+        start_date = f"2024-{month:02}-01"
+        basic_url = f"https://loteries.lotoquebec.com/en/lotteries/lotto-6-49?widget=resultats&action=historique&noproduit=212&date={start_date}"
+        response = requests.get(basic_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        dates = soup.find_all("div", class_="dateTirage")
+        for each_date in dates:
+            result.append(each_date.text)
+    return result
+
+
+def current_year():
+    saved_file = "current_winner.json"
+    data = dict()
+    dates = get_current_results_date()
+    for each_date in dates:
+        data[each_date] = dict()
+        base_url = f"https://loteries.lotoquebec.com/en/lotteries/lotto-6-49?date={each_date}"
+        response = requests.get(base_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        divs = soup.find_all("div", class_="lqZoneStructuresDeLots")
+        for i, each_div in enumerate(divs):
+            data[each_date][i] = each_div.text.strip()
+
+        with open(saved_file, "w") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    # history_year()
+    current_year()
